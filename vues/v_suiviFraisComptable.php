021ff747a2953fc91e9f248c4fa30a63adf93977
Louis-Marin MATHOREL <mathorel_louis@hotmail.fr>
2020-02-18 07:02:25 +0100

    chore - commit de mise à jour

---
+++ vues/v_suiviFraisComptable.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Vue supervision des mises en paiement du comptable
+ *
+ * PHP Version 7
+ *
+ * @category  PPE
+ * @package   GSB
+ * @author    Réseau CERTA <contact@reseaucerta.org>
+ * @author    Louis-Marin Mathorel <gsb2020@free.fr>
+ * @copyright 2017 Réseau CERTA
+ * @license   Réseau CERTA
+ * @version   GIT: <0>
+ * @link      http://gsb2020.org Contexte « Laboratoire GSB »
+ */
+?>
+
+<div class="row">
+	<h1>Supervision des mises en paiement des fiches</h1>
+</div>
+
