158dc24042526bdb8c4580d1caab9e2e1733435c
Louis-Marin MATHOREL <mathorel_louis@hotmail.fr>
2020-02-20 16:12:58 +0100

    bug : erreur de frappe

--- includes/fct.inc.php
+++ includes/fct.inc.php
@@ -272,7 +272,7 @@ function ajouterErreur($msg)
         $_REQUEST['erreurs'] = array();
     }
     $_REQUEST['erreurs'][] = $msg;
-    LogEvent('Erreur ("' . $msg . '") de ' . $_SESSION['prenom'] . ' ' . $_SESSION['nom'] . ' (IP = ' . $_SERVER['REMOTE_ADDR']);
+    addLogEvent('Erreur ("' . $msg . '") de ' . $_SESSION['prenom'] . ' ' . $_SESSION['nom'] . ' (IP = ' . $_SERVER['REMOTE_ADDR']);
 
 }
 
