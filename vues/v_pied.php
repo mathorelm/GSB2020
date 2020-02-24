a0b1406171e3de9ab05118fec8136cc39e92cdeb
Louis-Marin MATHOREL <mathorel_louis@hotmail.fr>
2020-02-24 21:36:16 +0100

    chore : modifications commentaires Doxygen

--- vues/v_pied.php
+++ vues/v_pied.php
@@ -5,8 +5,8 @@
  * PHP Version 7
  *
  * \package   GSB
- * \author    Réseau CERTA <contact\reseaucerta.org>
- * \author    José GIL <jgil\ac-nice.fr>
+ * \author    Réseau CERTA <contact@reseaucerta.org>
+ * \author    José GIL <jgil@ac-nice.fr>
  * \copyright 2017 Réseau CERTA
  * \license   Réseau CERTA
  * \version   GIT: <0>
--- vues/v_validFraisComptable.php
+++ vues/v_validFraisComptable.php
@@ -5,8 +5,8 @@
  * PHP Version 7
  *
  * \package   GSB
- * \author    Réseau CERTA <contact\reseaucerta.org>
- * \author    Louis-Marin Mathorel <gsb2020\free.fr>
+ * \author    Réseau CERTA <contact@reseaucerta.org>
+ * \author    Louis-Marin Mathorel <gsb2020@free.fr>
  * \copyright 2017 Réseau CERTA
  * \license   Réseau CERTA
  * \version   GIT: <0>
