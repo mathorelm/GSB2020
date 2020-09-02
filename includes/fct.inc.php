d5a468d2b076919ebb3312b6380bf061d19a4671
Louis-Marin MATHOREL <mathorel_louis@hotmail.fr>
2020-09-02 19:24:43 +0200

    fix : essais

--- includes/fct.inc.php
+++ includes/fct.inc.php
@@ -272,7 +272,7 @@ function ajouterErreur(string $msg)
         $_REQUEST['erreurs'] = array();
     }
     $_REQUEST['erreurs'][] = $msg;
-    if (session_status() == PHP_SESSION_ACTIVE) {
+    if (isset($_SESSION['nom'])) {
         addLogEvent(
             'Erreur ("' . $msg . '") de ' . $_SESSION['prenom'] . ' ' .
             $_SESSION['nom'] . ' (IP = ' . $_SERVER['REMOTE_ADDR']);
